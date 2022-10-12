fcdc1b476ba860cdd12a4c41e13294efd443ff80
Adam Rohrscheib <adamrohr@umich.edu>
2022-10-12 19:50:52 -0400

    Added background sound + bug fix

--- Assets/components/TreeGrowth.cs
+++ Assets/components/TreeGrowth.cs
@@ -6,7 +6,7 @@ public class TreeGrowth : MonoBehaviour
 {
 
     public float moneyEarned = 0.0f;
-    public float[] playerMovementGrowthTransition = [];
+    public float[] playerMovementGrowthTransition = {};
 
     // Start is called before the first frame update
     void Start()
