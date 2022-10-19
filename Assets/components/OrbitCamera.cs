498f59f64d8bf633678dda8e68d07c29c39e28a3
Adam Rohrscheib <adamrohr@umich.edu>
2022-10-19 19:35:20 -0400

    Camera Orbit drag on click done

--- Assets/components/OrbitCamera.cs
+++ Assets/components/OrbitCamera.cs
@@ -6,11 +6,30 @@ public class OrbitCamera : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    public float speed = 5.0f;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 desiredPosition = target.position + offset;
-        transform.position = desiredPosition;
+        // Vector3 desiredPosition = target.position + offset;
+        // transform.position = desiredPosition;
+        if(Input.GetMouseButton(0)) {
+            float translation = 0.0f;
+            if(Input.GetAxis("Mouse X") < 0) {
+                translation = -1;
+            } else if(Input.GetAxis("Mouse X") > 0) {
+                translation = 1;
+            }
+            // float zTranslation = Mathf.Sin(10 * Input.GetAxis("Mouse X"));
+            // float xTranslation = Mathf.Cos(10 * Input.GetAxis("Mouse X"));
+            float yTranslation = Input.GetAxis("Mouse Y") * speed;
+            if(transform.position.y > 0 || yTranslation > 0) {
+                transform.Translate(0, yTranslation, 0);
+            }
+            // float yTranslation = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+            transform.Translate(translation, 0, 0);
+            
+        }
+        transform.LookAt(target);
     }
 }
